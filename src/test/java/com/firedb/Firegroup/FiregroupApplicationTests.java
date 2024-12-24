66c1eb3fa784f36c2fb033c270e072c602e12d44
Petroni-bup <66560510+Petroni-bup@users.noreply.github.com>
2024-12-24 16:20:01 +0200

    refactoring

--- src/test/java/com/firedb/Firegroup/FiregroupApplicationTests.java
+++ src/test/java/com/firedb/Firegroup/FiregroupApplicationTests.java
@@ -2,7 +2,6 @@ package com.firedb.Firegroup;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @DataJpaTest
